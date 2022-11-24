ac498438432dc3437080de4a2e35fa27e7a06796
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-11-24 17:28:28 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config()
 
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4lwt8qz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
 
 
 
