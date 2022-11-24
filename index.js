890408f75d662c04293c8baef92ede5bacf5a49f
MohammadSazzatulIslam <rockstarwds@gmail.com>
2022-11-24 21:16:36 +0600

    add category api

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4lwt8qz.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -19,14 +18,33 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+ function run() {
+  try {
+    
+    const categoryCollection = client.db("watchWerehouse").collection('categories')
+
+    app.get("/", (req, res) => {
+      res.send("watches api is comming soon");
+    });
+
+
+    app.get('/category', async(req, res) => {
+      const filter = {}
+      const result = await categoryCollection.find(filter).toArray()
+      res.send(result)
+    })
 
 
 
 
+  } finally {
+    
+  }
+}
 
-app.get('/', (req,res) => {
-    res.send('watches api is comming soon')
-})
+run()
+
+
 
 
 app.listen(port ,()=> {
